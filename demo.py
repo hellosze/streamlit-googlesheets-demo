782e399c46c86644b95afa0f08ee7320ea95448f
sze chan <sze@hello-sze.com>
2024-10-25 13:13:42 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,14 +1,26 @@
-# streamlit_app.py
+# # streamlit_app.py
+
+# import streamlit as st
+# from streamlit_gsheets import GSheetsConnection
+
+# # Create a connection object.
+# conn = st.connection("gsheets", type=GSheetsConnection)
+
+# df = conn.read(
+#     worksheet="Sheet1",
+#     ttl="10m",
+#     usecols=[0, 1],
+#     nrows=3,
+# )
+
+# example/st_app.py
 
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
+url = "https://docs.google.com/spreadsheets/d/1cpwK088ub8LCVNTo6hQYsBTZstJX026h9zZiS-tLg1U/edit?gid=71029213#gid=71029213"
+
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(
-    worksheet="Sheet1",
-    ttl="10m",
-    usecols=[0, 1],
-    nrows=3,
-)
+data = conn.read(spreadsheet=url, usecols=[0, 1])
+st.dataframe(data)
