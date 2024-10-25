b41b3acf53f7a547b7a96b210033903a2e9c895a
sze chan <sze@hello-sze.com>
2024-10-25 13:24:31 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -18,8 +18,8 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-# url = "https://docs.google.com/spreadsheets/d/1cpwK088ub8LCVNTo6hQYsBTZstJX026h9zZiS-tLg1U/edit?gid=71029213#gid=71029213"
-url = "https://docs.google.com/spreadsheets/d/1LZ4bPm1lVFfSTc8GOj98Nzo1uWgDbb2QaB451vohtT8/edit?gid=0#gid=0"
+url = "https://docs.google.com/spreadsheets/d/1cpwK088ub8LCVNTo6hQYsBTZstJX026h9zZiS-tLg1U/edit?gid=71029213#gid=71029213"
+# url = "https://docs.google.com/spreadsheets/d/1LZ4bPm1lVFfSTc8GOj98Nzo1uWgDbb2QaB451vohtT8/edit?gid=0#gid=0"
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 data = conn.read(spreadsheet=url, usecols=[0, 1])
