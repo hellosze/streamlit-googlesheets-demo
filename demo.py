3c150a0036153bc68a195d2f8898683533e445ee
sze chan <sze@hello-sze.com>
2024-10-29 22:10:31 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("Crain's Sponsorship_House Line Item ends 11_30_2024 (refresh=true).csv",thousands=',')
+df = pd.read_csv("Crain's Sponsorship_House Line Item ends 11_30_2024 (refresh=true).csv",thousands=',')[:-1]
 # df['Total impressions'] = df['Total impressions'].fillna(0).astype(int)
 
 
