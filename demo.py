4db12cf40f819c1e12c2a8e673566e95be3aacfa
sze chan <sze@hello-sze.com>
2024-11-11 07:39:00 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+### add new file here
 filename = "crains_nov10.csv"
 # filename = "Crain's Sponsorship_House Line Item ends 11_30_2024 (refresh=true) (Oct 5, 2024 - Nov 3, 2024) - Crain's Sponsorship_House Line Item ends 11_30_2024 (refresh=true) (Oct 5, 2024 - Nov 3, 2024).csv"
 # filename = "Crain's Sponsorship_House Line Item ends 11_30_2024 (refresh=true) (Oct 2, 2024 - Oct 31, 2024) (1) - Crain's Sponsorship_House Line Item ends 11_30_2024 (refresh=true) (Oct 2, 2024 - Oct 31, 2024) (1).csv"
