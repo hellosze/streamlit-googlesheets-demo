453caf89ad6e5d0d49409ea180208d7a663c5d2b
sze chan <sze@hello-sze.com>
2024-11-11 07:49:56 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -20,6 +20,8 @@ df['Total impressions'] = df['Total impressions'].apply(convert_to_int)
 #different aggregate functions
 table = pd.pivot_table(df,index=['Date'],aggfunc={'Total impressions':np.sum})
 table.sort_index(inplace=True)
+table.sort_values(by='Date', axis=0, inplace=True)
+
 st.write(table)
 st.line_chart(table)
 
